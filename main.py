8e9d1c4cbf0253ceae9d09fe478d66867480c599
vba1946 <vba1946@gmail.com>
2025-07-25 11:26:27 +0300

    add new8

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def area_circle(r):
-    s = 3.14 * r ** 2
-    return s
-
-def area_square_area(a):
-    S_s = a ** 2
-    return S_s
-
-def area_trapezoid(a, b, h):
-    S_t = 0.5 * (a + b) * h
-    return S_t
-
+def calc(a, b):
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    print(a/b)
+    
