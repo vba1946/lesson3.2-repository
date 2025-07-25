f9eb22ac0163568fd07f72b605638f2f92d82324
vba1946 <vba1946@gmail.com>
2025-07-25 12:28:01 +0300

    add new func summa add new file test

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-def calc(a, b):
-    print(a + b)
-    print(a - b)
-    print(a * b)
-    print(a/b)
+def summa(a, b):
+    print(a+b)
     
---
+++ test.py
@@ -0,0 +1,5 @@
+from main import summa
+
+summa(3, 5)
+summa(56, 1)
+summa(67, 5)
